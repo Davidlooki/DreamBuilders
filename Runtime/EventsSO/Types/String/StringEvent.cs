8443d310130d3141f8710cbc6e3d8c0e1581fb10
David Barcelos dos Santos Vieira <davidlooki@hotmail.com>
2022-05-16 15:00:50 -0300

    Implementes StringEventSO

---
+++ Runtime/EventsSO/Types/String/StringEvent.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New String Event", menuName = "DreamBuilders/Game Events/String Event", order = 0)]
+public class StringEvent : BaseGameEvent<string> { }
