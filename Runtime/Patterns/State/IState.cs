af5af38251aaa0772f971a283d07deaeebf74fb4
Davidlooki <davidlooki@hotmail.com>
2022-07-26 11:34:10 -0300

    Updated State pattern, included generic

--- Runtime/Patterns/State/IState.cs
+++ Runtime/Patterns/State/IState.cs
@@ -14,4 +14,19 @@ namespace DreamBuilders
         //Automaticly called in State Machine. Allows delay flow if desired.
         public void Exit(IStateContext context);
     }
+
+    public interface IState<in T> where T : IStateContext<T>
+    {
+        //Automaticly called in State Machine. Allows delay flow if desired.
+        public void Enter(T context);
+
+        //Allows simulation of Update without MonoBehaviour attached.
+        public void Tick(T context);
+
+        //Allows simulation of FixedUpdate without MonoBehaviour attached.
+        public void FixedTick(T context);
+
+        //Automaticly called in State Machine. Allows delay flow if desired.
+        public void Exit(T context);
+    }
 }
