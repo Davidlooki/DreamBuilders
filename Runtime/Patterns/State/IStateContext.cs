235be551d2e61481bff8614ed95110bb28edc238
Davidlooki <davidlooki@hotmail.com>
2022-07-29 13:48:08 -0300

    Update IStateContext.cs

--- Runtime/Patterns/State/IStateContext.cs
+++ Runtime/Patterns/State/IStateContext.cs
@@ -1,22 +1,5 @@
 namespace DreamBuilders
 {
-    public interface IStateContext<T> where T : IStateContext<T>
-    {
-        #region Fields
-
-        public IState<T> CurrentState { get; }
-        public IState<T> PreviousState { get; }
-
-        #endregion
-
-        #region Methods
-
-        public void SetState(IState<T> state);
-        public void RevertState();
-
-        #endregion
-    }
-    
     public interface IStateContext
     {
         #region Fields
@@ -29,6 +12,23 @@ namespace DreamBuilders
         #region Methods
 
         public void SetState(IState state);
+        public void RevertState();
+
+        #endregion
+    }
+
+    public interface IStateContext<T> where T : IStateContext<T>
+    {
+        #region Fields
+
+        public IState<T> CurrentState { get; }
+        public IState<T> PreviousState { get; }
+
+        #endregion
+
+        #region Methods
+
+        public void SetState(IState<T> state);
         public void RevertState();
 
         #endregion
